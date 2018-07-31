ef1bd9c13ba5c442271c39ca877c59f5e0245757
Kyle Rudeen <krudeen@gmail.com>
2018-07-31 20:42:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function theBeatlesPlay(musicians, instruments){
    var x = [];
    
    for (let i=0; i<musicians.length; i++) {
-  x.push(musicians[i] + "plays" + instruments[i]);
+  x.push(musicians[i] + " plays " + instruments[i]);
 }
 
 return x;
